851e34bbacb644f80ba22807f3b7da428e64cfd6
Tahasin Raza Khan <tahasinrazakhan7@gmail.com>
2022-05-28 21:45:03 +0530

    first commit

---
+++ client/client/src/App.js
@@ -0,0 +1,19 @@
+// import Home from "./pages/home/Home";
+// function App() {
+//   return <Home/>
+// }
+
+// export default App;
+ 
+
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Profile from "./pages/profile/Profile";
+// import Register from "./pages/register/Register";
+
+function App() {
+  // return <Profile/>;
+  return <Home/>;
+}
+
+export default App;
